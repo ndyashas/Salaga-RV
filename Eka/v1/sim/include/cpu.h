4e748fd9f1470c86f2547f4e000134ca404da71c
Yashas ND <ndyashas@gmail.com>
2021-12-26 18:44:03 -0500

    Add VCD file generation support
    Added generation of VCD file support to the template class
    again, inspired by:
    
    https://zipcpu.com/blog/2017/06/21/looking-at-verilator.html

--- Eka/v1/sim/include/cpu.h
+++ Eka/v1/sim/include/cpu.h
@@ -8,28 +8,38 @@
 #include <iostream>
 #include <stdlib.h>
 #include "verilated.h"
+#include <verilated_vcd_c.h>
+
 
 template<class CPU_mod>
 class CPU {
 	unsigned long int tick_count;
 	unsigned long int total_sim_ticks;
 	CPU_mod *cpu_mod;
+	VerilatedVcdC *tracer;
+	bool trace;
 
 public:
-	CPU(unsigned long int total_sim_ticks = 100);
+	CPU(unsigned long int total_sim_ticks = 100, bool trace = true);
 	~CPU(void);
 
 	void reset(void);
 	void tick(void);
+	void open_trace(const char *vcd_file_name);
+	void close_trace(void);
 	bool done(void);
 };
 
 template<class CPU_mod>
-CPU<CPU_mod>::CPU(unsigned long int total_sim_ticks)
+CPU<CPU_mod>::CPU(unsigned long int total_sim_ticks, bool trace)
 {
 	this->cpu_mod = new CPU_mod;
 	this->tick_count = 0l;
 	this->total_sim_ticks = total_sim_ticks;
+	this->trace = trace;
+	if (trace) {
+		Verilated::traceEverOn(true);
+	}
 }
 
 template<class CPU_mod>
@@ -54,11 +64,51 @@ void CPU<CPU_mod>::tick(void)
 
 	this->cpu_mod->clk = 0;
 	this->cpu_mod->eval();
+
+	if (this->trace && this->tracer) tracer->dump(10*this->tick_count-2);
+
 	this->cpu_mod->clk = 1;
 	this->cpu_mod->eval();
+
+	if (this->trace && this->tracer) tracer->dump(10*this->tick_count);
+
 	this->cpu_mod->clk = 0;
 	this->cpu_mod->eval();
+
+	if (this->trace && this->tracer) {
+		tracer->dump(10*this->tick_count+5);
+		tracer->flush();
+	}
 }
+
+template<class CPU_mod>
+void CPU<CPU_mod>::open_trace(const char *vcd_file_name)
+{
+	if (this->trace) {
+		if (!tracer) {
+			this->tracer = new VerilatedVcdC;
+			this->cpu_mod->trace(this->tracer, 99);
+			this->tracer->open(vcd_file_name);
+		}
+		else {
+			std::cout
+				<< "Unable to initialize tracing, VCD file will NOT be generated"
+				<< std::endl;
+		}
+	}
+}
+
+template<class CPU_mod>
+void CPU<CPU_mod>::close_trace(void)
+{
+	if (this->trace) {
+		if (tracer) {
+			tracer->close();
+			tracer = NULL;
+		}
+	}
+}
+
 
 template<class CPU_mod>
 bool CPU<CPU_mod>::done(void)
--- Eka/v1/sim/src/main_sim.cpp
+++ Eka/v1/sim/src/main_sim.cpp
@@ -9,10 +9,12 @@ int main(int argc, char **argv)
 	Verilated::commandArgs(argc, argv);	
 
 	CPU<Veka_core_v1> *core = new CPU<Veka_core_v1>(100);
-	
+
+	core->open_trace("eka_core_v1_sim.vcd");
 	while(!core->done()) {
 		core->tick();
 	}
+	core->close_trace();
 
 	return 0;
 }
