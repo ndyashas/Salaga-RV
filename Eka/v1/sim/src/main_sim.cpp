df75ab6262eb337a0467e24f762e6185b528a6ad
Yashas ND <ndyashas@gmail.com>
2021-12-30 22:25:12 -0500

    Change debug instruction
    Changed the debug instruction to be more simpler.
    This helps in implementing the different parts of the CPU.

--- Eka/v1/sim/src/main_sim.cpp
+++ Eka/v1/sim/src/main_sim.cpp
@@ -12,17 +12,15 @@ int main(int argc, char **argv)
 	unsigned int* l1_inst_cache = new unsigned int[1024];
 
 	l1_inst_cache[0]  = 0b00000000000000000000000000000000;
-	l1_inst_cache[1]  = 0b00000000101000000010000010000011; // LW rd(1) rs1(0) 10
-	l1_inst_cache[2]  = 0b00000000101100000010000100000000; // LW rd(2) rs1(0) 11
+	l1_inst_cache[1]  = 0b00000000010100000000000010010011; // ADDI rd(1) rs1(0) 5
+	l1_inst_cache[2]  = 0b00000000011100000000000100010011; // ADDI rd(2) rs1(0) 7
 	l1_inst_cache[3]  = 0b00000000001000001000000110110011; // ADD rd(3) rs1(1) rs1(2)
-	l1_inst_cache[4]  = 0b00000000000000011010011000100011; // SW rs1(3) rs2(0) 12
+	l1_inst_cache[4]  = 0b00000000000000011010010100100011; // SW rs1(3) rs2(0) 10
 
 	unsigned int* l1_data_cache = new unsigned int[4096];
 
-	l1_data_cache[9]  = 0b00000000000000000000000000000101; // 5
-	l1_data_cache[10] = 0b00000000000000000000000000000111; // 7
-	l1_data_cache[11] = 0b00000000000000000000000000000000; // expect 12
-	
+	l1_data_cache[9]  = 0b00000000000000000000000000000000; // expect 12
+
 	Memory_controller* memory_controller = new Memory_controller(l1_inst_cache,
 								     1024/4,
 								     l1_data_cache,
