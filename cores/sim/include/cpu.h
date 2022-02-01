5b8dac007fa58a993dae804f6eac4f31992f62dc
Yashas ND <ndyashas@gmail.com>
2022-01-31 23:14:20 -0800

    Move memory abstraction code to cpu.h instead of main_sim.cpp

--- cores/sim/include/cpu.h
+++ cores/sim/include/cpu.h
@@ -6,9 +6,14 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include "verilated.h"
 #include <verilated_vcd_c.h>
+#include "Vsalaga_chip.h"
+#include "Vsalaga_chip___024root.h"
+#include "misc.h"
+
 
 #ifndef __SALAGA_CORE__
 #define __SALAGA_CORE__
@@ -22,12 +27,15 @@ private:
 	VerilatedVcdC *tracer;
 	bool trace;
 	bool debug_print;
+	void __update_access_buffer();
 
 public:
 	CPU_mod *cpu_mod;
 	/* Set 'total_sim_ticks' to 0 for unbound simulation till
 	 * 'Ctrl + C' is pressed.
 	 */
+	uint32_t* access_buffer;
+
 	CPU(unsigned long int total_sim_ticks = 100,
 	    // Memory_controller* memory_controller = NULL,
 	    bool trace = true,
@@ -39,6 +47,9 @@ public:
 	void open_trace(const char *vcd_file_name);
 	void close_trace(void);
 	bool done(void);
+	void load_cache(int cache_type,
+			unsigned long int l1_cache_size_in_words,
+			const char* hex_file_path);
 };
 
 template<class CPU_mod>
@@ -60,12 +71,15 @@ CPU<CPU_mod>::CPU(unsigned long int total_sim_ticks,
 	}
 	this->tracer = NULL;
 	this->debug_print = debug_print;
+
+	this->access_buffer = new uint32_t[512];
 }
 
 template<class CPU_mod>
 CPU<CPU_mod>::~CPU(void)
 {
 	delete this->cpu_mod;
+	delete[] this->access_buffer;
 	this->cpu_mod = NULL;
 }
 
@@ -94,6 +108,8 @@ void CPU<CPU_mod>::tick(void)
 
 	this->cpu_mod->clk = 0;
 	this->cpu_mod->eval();
+
+	this->__update_access_buffer();
 
 	if (this->trace && this->tracer) {
 		this->tracer->dump(10*this->tick_count+5);
@@ -135,4 +151,50 @@ bool CPU<CPU_mod>::done(void)
 		|| ((this->total_sim_ticks != 0) && (this->tick_count >= this->total_sim_ticks)));
 }
 
+template<class CPU_mod>
+void CPU<CPU_mod>::load_cache(int cache_type,
+			      unsigned long int l1_cache_size_in_words,
+			      const char* hex_file_path)
+{
+	unsigned long int i;
+	uint32_t word;
+	std::ifstream hex_file(hex_file_path);
+
+	i = 0;
+	while((i < (int)(CACHE_SIZE/4)) && (hex_file >> std::hex >> word)) {
+		if (cache_type == I_CACHE) {
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_3[i]   = (word & 0xff000000) >> 24;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_2[i]   = (word & 0x00ff0000) >> 16;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_1[i]   = (word & 0x0000ff00) >> 8;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_0[i++] = (word & 0x000000ff);
+		}
+		else if (cache_type == D_CACHE) {
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_3[i]   = (word & 0xff000000) >> 24;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_2[i]   = (word & 0x00ff0000) >> 16;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_1[i]   = (word & 0x0000ff00) >> 8;
+		        this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_0[i++] = (word & 0x000000ff);
+		}
+	}
+
+	hex_file.close();
+}
+
+
+template<class CPU_mod>
+void CPU<CPU_mod>::__update_access_buffer()
+{
+	int i;
+
+	for(i = 0; i < 512; ++i) {
+		this->access_buffer[i] = 0;
+		this->access_buffer[i] = (this->access_buffer[i]
+					  | ((this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_3[2048+i] << 24) & 0xff000000));
+		this->access_buffer[i] = (this->access_buffer[i]
+					  | ((this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_2[2048+i] << 16) & 0x00ff0000));
+		this->access_buffer[i] = (this->access_buffer[i]
+					  | ((this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_1[2048+i] <<  8) & 0x0000ff00));
+		this->access_buffer[i] = (this->access_buffer[i]
+					  | (this->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_0[2048+i] & 0x000000ff));
+	}
+}
 #endif
--- cores/sim/src/main_sim.cpp
+++ cores/sim/src/main_sim.cpp
@@ -7,12 +7,6 @@
 #include "cpu.h"
 #include "misc.h"
 
-void load_cache(CPU<Vsalaga_chip>* core,
-		int cache_type,
-		unsigned long int l1_cache_size_in_words,
-		const char* hex_file_path);
-
-
 int main(int argc, char **argv)
 {
 	Verilated::commandArgs(argc, argv);
@@ -22,50 +16,23 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	CPU<Vsalaga_chip>* core = new CPU<Vsalaga_chip>(2000,
+	CPU<Vsalaga_chip>* core = new CPU<Vsalaga_chip>(10000,
 						  true,
 						  false);
 
-	load_cache(core, I_CACHE, CACHE_SIZE/4, argv[1]);
-	load_cache(core, D_CACHE, CACHE_SIZE/4, argv[1]);
+	core->load_cache(I_CACHE, CACHE_SIZE/4, argv[1]);
+	core->load_cache(D_CACHE, CACHE_SIZE/4, argv[1]);
 
 	core->open_trace("salaga_chip_sim.vcd");
 	core->reset();
+
 	while(!core->done()) {
 		core->tick();
 	}
+
 	core->close_trace();
 
 	delete core;
 
 	return 0;
-}
-
-	
-void load_cache(CPU<Vsalaga_chip>* core,
-		int cache_type,
-		unsigned long int l1_cache_size_in_words,
-		const char* hex_file_path)
-{
-	unsigned long int i;
-	unsigned int word;
-	std::ifstream hex_file(hex_file_path);
-
-	i = 0;
-	while((i < (int)(CACHE_SIZE/4)) && (hex_file >> std::hex >> word)) {
-		if (cache_type == I_CACHE) {
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_3[i]   = (word & 0xff000000) >> 24;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_2[i]   = (word & 0x00ff0000) >> 16;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_1[i]   = (word & 0x0000ff00) >> 8;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_inst_cache__DOT__l1_memory_bank_0[i++] = (word & 0x000000ff);
-		}
-		else if (cache_type == D_CACHE) {
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_3[i]   = (word & 0xff000000) >> 24;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_2[i]   = (word & 0x00ff0000) >> 16;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_1[i]   = (word & 0x0000ff00) >> 8;
-		        core->cpu_mod->rootp->salaga_chip__DOT__l1_data_cache__DOT__l1_memory_bank_0[i++] = (word & 0x000000ff);
-		}
-	}
-
-	hex_file.close();
 }
