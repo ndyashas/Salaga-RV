f118933a4e7d3c334c64535b6cf48aaa627ed96e
Yashas ND <ndyashas@gmail.com>
2022-11-12 21:01:06 -0800

    Refactor project

--- cores/sim/include/misc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SALAGA_MISC__
-#define __SALAGA_MISC__
-
-#define CACHE_SIZE 16 * 1024 /* 2K In bytes for each instruction and data caches */
-
-#define I_CACHE 0
-#define D_CACHE 1
-
-#endif
--- cores/sim/test-sw/common/startup-script.c
+++
@@ -1,6 +0,0 @@
-int main(void);
-
-void reset_handler()
-{
-	main();
-}
