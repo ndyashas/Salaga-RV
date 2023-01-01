c56c0ff35aad8bf5b11a5a34187fa9e7ec90616d
Yashas ND <ndyashas@gmail.com>
2023-01-01 19:16:36 +0530

    Change UART writes from lazy to eager

--- firmware/salagalib/UART.s
+++ firmware/salagalib/UART.s
@@ -5,9 +5,9 @@
 
 UART_putchar:
 	li t0, _UART_MMIO_BASE_ADDR
+	sw a0, 0(t0)
 wait_to_send:
 	lw t1, 0(t0)
 	andi t1, t1, 256 # 9th bit is the busy bit.
 	bne t1, x0, wait_to_send
-	sw a0, 0(t0)
 	ret
