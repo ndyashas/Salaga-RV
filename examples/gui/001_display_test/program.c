3238d48f8fb843f2ac7de09d9f4dd141c58e9ce7
Yashas ND <ndyashas@gmail.com>
2023-01-09 00:34:51 +0530

    Update display test workload

--- examples/gui/001_display_test/program.c
+++ examples/gui/001_display_test/program.c
@@ -2,9 +2,13 @@
 #include <string.h>
 #include <salagalib.h>
 #include <salagagl.h>
+#include <salagaio.h>
 
 #define PINNED_ARRAY_SIZE 20
 unsigned int pinned_array[PINNED_ARRAY_SIZE] __attribute__((section(".pinned_array_section")));
+
+#define MIN(X, Y)  (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y)  (((X) > (Y)) ? (X) : (Y))
 
 int main()
 {
@@ -14,10 +18,13 @@ int main()
     Color red = {255, 0, 0};
     Color green = {0, 255, 0};
     Color blue = {0, 0, 255};
+    Color white = {255, 255, 255};
+    Color black = {0, 0, 0};
 
     Color color_array[3] = {red, green, blue};
 
     ///--------------------- Full screen color - set_pixel -----------------------
+    print_str("Full screen coloring based on 'set_pixel'\0");
     for (i = 0; i < 3; ++i) {
 	color_i = color_array[i];
 	for (row = 0; row < SALAGA_DISPLAY_HEIGHT; ++row) {
@@ -29,6 +36,7 @@ int main()
     }
 
     ///--------------------- Full screen color - draw_line ---- ------------------
+    print_str("Full screen coloring based on 'draw_line'\0");
     for (i = 0; i < 3; ++i) {
 	color_i = color_array[i];
 	for (row = 0; row < SALAGA_DISPLAY_HEIGHT; ++row) {
@@ -38,11 +46,21 @@ int main()
     }
 
     ///--------------------- Full screen color - draw_rectangle ------------------
+    print_str("Full screen coloring based on 'draw_rectangle'\0");
     for (i = 0; i < 3; ++i) {
 	color_i = color_array[i];
 	draw_rectangle(0, 0, SALAGA_DISPLAY_WIDTH, SALAGA_DISPLAY_HEIGHT, color_i);
 	sleep(100);
     }
+    draw_rectangle(0, 0, SALAGA_DISPLAY_WIDTH, SALAGA_DISPLAY_HEIGHT, white);
+
+    ///--------------------- Full screen color - draw_rectangle ------------------
+    print_str("Pattern printing using 'draw_line'\0");
+    draw_line(0, 0, SALAGA_DISPLAY_WIDTH-1, SALAGA_DISPLAY_HEIGHT-1, black);
+    draw_line(0, SALAGA_DISPLAY_HEIGHT-1, SALAGA_DISPLAY_WIDTH-1, 0, black);
+    draw_line(SALAGA_DISPLAY_WIDTH/2, 0, SALAGA_DISPLAY_WIDTH/2, SALAGA_DISPLAY_HEIGHT-1, black);
+    draw_line(0, SALAGA_DISPLAY_HEIGHT/2, SALAGA_DISPLAY_WIDTH-1, SALAGA_DISPLAY_HEIGHT/2, black);
+    sleep(75);
 
     return 0;
 }
